1e2bf918dcfd973495842fe1155ba6af91121286
DTS0 <dreamoftime0@gmail.com>
2018-06-18 18:32:35 +0800

    update README

--- test.cpp
+++ test.cpp
@@ -1,12 +1,19 @@
 #include<iostream>
 #include<stdlib.h>
 #include<string>
-#include"include/reply.h"
+#include"reply.h"
+#include"skill.h"
+#include"play.h"
+#include"record.h"
+#include"voice2word.h"
+#include"word2voice.h"
 
 using namespace std;
 
 int main(int argc,char* argv[]){
 	replyInit();
+	skillInit();
+	
 	//cout<<argc<<argv[1];
 	if(argv[1]!=NULL)
         cout<<reply(argv[1]);
