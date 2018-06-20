a8d7e538318601a49cebd56de81df8eba9411e17
DTS0 <dreamoftime0@gmail.com>
2018-06-20 20:51:20 +0800

    add some describe and delete some useless files

--- delNeedlessH.sh
+++ delNeedlessH.sh
@@ -2,3 +2,7 @@ cd include/json
 DELS=$(echo *)
 cd ..
 rm -rf $DELS
+
+#this file will be used when make copy_h.
+#this file must be strange,but I can't find a better way to delete the .h files in json.If I don't delete them from include,there will be some errors.Maybe it's because of conflicts of the .h files.
+#this still work well.If you have a better way to make it,just modify this file and the makefile.
